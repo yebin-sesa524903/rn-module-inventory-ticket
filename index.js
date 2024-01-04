74c9a232a19393d22b442501d462b9b089a4112d
liuqing <330663384@qq.com>
2024-01-04 22:38:21 +0800

    更新customerId入口

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React,{} from 'react'
 import TicketList from './app/TicketList'
-import {configCookie} from './app/middleware/bff'
+import {configCookie, updateInventoryCustomerId} from './app/middleware/bff'
 
 export  {
-  TicketList,configCookie
+  TicketList,configCookie, updateInventoryCustomerId
 }
 
 
